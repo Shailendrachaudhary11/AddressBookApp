19facef829eabf3bfd18e3d4a229c0180d229e6c
Shailendrachaudhary11 <shailendrachaurdhary0008@gmail.com>
2025-03-17 16:48:15 +0530

    UC6 lombok

--- src/main/java/com/AddressBookApp/dto/ContactDTO.java
+++ src/main/java/com/AddressBookApp/dto/ContactDTO.java
@@ -1,14 +1,17 @@
 package com.AddressBookApp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ContactDTO {
-    private Long id;
+    //private Long id;
     private String name;
-    private String phone;
+    //private String phone;
     private String email;
-    private String address;
+    //private String address;
 }
--- src/main/java/com/AddressBookApp/model/Contact.java
+++ src/main/java/com/AddressBookApp/model/Contact.java
@@ -17,5 +17,5 @@ public class Contact {
     private String name;
     private String phone;
     private String email;
-    private String address;
+//    private String address;
 }
